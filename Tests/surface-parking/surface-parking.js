31c511dbb90a442685f7289916a7c2c6af26632a
NOHELIA EUCEDA <neuceda@HN-HNSC00396M.loe.corp>
2020-06-27 00:28:09 -0600

    Surface parking done

---
+++ Tests/surface-parking/surface-parking.js
@@ -0,0 +1,104 @@
+var ParkingPage = require('../../Pages/parking-calculator/parking-page');
+var CalendarPage = require('../../Pages/parking-calculator/calendar-page');
+describe('Protractor Demo App', function () {
+    var originalTimeout;
+
+
+    beforeEach(function () {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000;
+        browser.ignoreSynchronization = true;
+        browser.get('http://www.shino.de/parkcalc/');
+    });
+
+    afterEach(function () {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
+    it('Given as a user, When the user enters the date in the date field AND the user spends two hours in Economy parking, THEN parking cost equals to $4'
+        , async (done) => {
+            ParkingPage.chooseParking('Long-Surface')
+            ParkingPage.selectStartDate()
+            CalendarPage.clickMonth()
+            CalendarPage.selectMonth(6)
+            CalendarPage.clickStartDate(25)
+            ParkingPage.typeStartHour('5:00')
+            ParkingPage.startHour()
+            ParkingPage.selectEndDate()
+            CalendarPage.clickMonth()
+            CalendarPage.selectMonth(6)
+            CalendarPage.clickStartDate(25)
+            ParkingPage.typeEndHour('7:00')
+            ParkingPage.endHour()
+            ParkingPage.calculateClick()
+            ParkingPage.verifyCost('$ 4.00')
+
+
+            done()
+        });
+    it('Given as a user, When the user enters the date in the date field AND the user spends minus two hours in valet parking, THEN parking cost equals to $0'
+        , async (done) => {
+            ParkingPage.chooseParking('Long-Surface')
+            ParkingPage.selectStartDate()
+            CalendarPage.clickMonth()
+            CalendarPage.selectMonth(6)
+            CalendarPage.clickStartDate(25)
+            ParkingPage.typeStartHour('5:00')
+            ParkingPage.startHour()
+            ParkingPage.selectEndDate()
+            CalendarPage.clickMonth()
+            CalendarPage.selectMonth(6)
+            CalendarPage.clickStartDate(25)
+            ParkingPage.typeEndHour('2:30')
+            ParkingPage.endHour()
+            ParkingPage.calculateClick()
+            ParkingPage.verifyCost('$ 0.00')
+
+
+            done()
+        });
+    it('Given as a user, When the user enters the date in the date field AND the user spends minus two hours in valet parking, THEN parking cost equals to $0'
+        , async (done) => {
+            ParkingPage.chooseParking('Long-Surface')
+            ParkingPage.selectStartDate()
+            CalendarPage.clickMonth()
+            CalendarPage.selectMonth(6)
+            CalendarPage.clickStartDate(17)
+            ParkingPage.typeStartHour('5:00')
+            ParkingPage.startHour()
+            ParkingPage.selectEndDate()
+            CalendarPage.clickMonth()
+            CalendarPage.selectMonth(6)
+            CalendarPage.clickStartDate(24)
+            ParkingPage.typeEndHour('5:00')
+            ParkingPage.endHour()
+            ParkingPage.calculateClick()
+            ParkingPage.verifyCost('$ 60.00')
+
+
+            done()
+        });
+        it('Given as a user, When the user enters the date in the date field AND the user spends minus two hours in valet parking, THEN parking cost equals to $0'
+        , async (done) => {
+            ParkingPage.chooseParking('Long-Surface')
+            ParkingPage.selectStartDate()
+            CalendarPage.clickMonth()
+            CalendarPage.selectMonth(6)
+            CalendarPage.clickStartDate(17)
+            ParkingPage.typeStartHour('5:00')
+            ParkingPage.startHour()
+            ParkingPage.selectEndDate()
+            CalendarPage.clickMonth()
+            CalendarPage.selectMonth(6)
+            CalendarPage.clickStartDate(18)
+            ParkingPage.typeEndHour('6:00')
+            ParkingPage.endHour()
+            ParkingPage.calculateClick()
+            ParkingPage.verifyCost('$ 12.00')
+
+
+            done()
+        });
+    afterAll(function () {
+        browser.restartSync()
+    })
+});
--- conf.js
+++ conf.js
@@ -3,33 +3,33 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: [
-     //'./Tests/valet-parking/valet-parking.js',
-     //'./Tests/short-parking/short-parking.js'
-    //  /'./Tests/economy-parking/economy-parking.js'
+    //'./Tests/valet-parking/valet-parking.js',
   ],
   suites: {
-    //ValetParkig: './Tests/valet-parking/valet-parking.js',
-    //ShortParkig: './Tests/short-parking/short-parking.js',
-    EconomyParkig: './Tests/economy-parking/economy-parking.js'
+    ValetParkig: './Tests/valet-parking/valet-parking.js',
+    ShortParkig: './Tests/short-parking/short-parking.js',
+    EconomyParkig: './Tests/economy-parking/economy-parking.js',
+    GarageParkig: './Tests/garage-parking/garage-parking.js',
+    SurfaceParkig: './Tests/surface-parking/surface-parking.js'
   },
-  
+
   multiCapabilities: [{
-    browserName: 'chrome' , 
+    browserName: 'chrome',
     trustAllSSLCertificates: true,
     acceptInsecureCerts: true,
     ACCEPT_SSL_CERTS: true,
     shardTestFiles: true,
     maxInstances: 1,
     chromeOptions: {
-      args: [ 
+      args: [
         //'incognito',
-         "--disable-gpu",
-      //'--headless',
-       "--window-size=1600,900" ]
+        "--disable-gpu",
+        //'--headless',
+        "--window-size=1600,900"]
     }
   }],
   jasmineNodeOpts: {
     showColors: true, // Use colors in the command line report.
   }
-  
+
 }
